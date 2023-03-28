d21370b5aab2133ed5496a20d465c837c63bb166
YUVRAJ KUMAR ARORA <110113664+Officialyuvi070@users.noreply.github.com>
2023-03-28 19:47:43 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,37 @@
+# Type Casting in Python
+
+# Type Casting Int to Float
+a = 12 # int variable
+b = float(a)
+print(b)
+print(type(b))
+
+# Type Casting Int to String
+x = 12 # int variable
+y = str(x)
+print(y)
+print(type(y))
+      
+# Type Casting Float to Int
+c = 80.84 # float variable
+d = int(c)
+print(d)
+print(type(d))
+
+# Type Casting Float to string
+p = 56.34 # float variable
+q = str(p)
+print(q)
+print(type(q))
+
+# Type Casting String to Int
+j = "786" # string variable
+k = str(j)
+print(k)
+print(type(k))
+
+# Type Casting String to Float
+m = "45.54" # string variable
+n = float(m)
+print(n)
+print(type(n))
